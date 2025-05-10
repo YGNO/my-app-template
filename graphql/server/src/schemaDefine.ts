10d029fcba689cc2ec1e504d20da42d14fe3ec2b
naoki.okamoto <ygkiyo@okana.jp>
2025-05-10 18:23:03 +0900

    コメント位置調整

--- graphql/server/src/schemaDefine.ts
+++ graphql/server/src/schemaDefine.ts
@@ -1,28 +1,31 @@
 import { dbClient, dbSchema } from "@my-app/db-client";
 import { buildSchema } from "drizzle-graphql";
 import { printSchema } from "graphql";
-import { type SchemaBuilderType, getSchemaBuilder } from "./schemaBuilder.ts";
+import { getSchemaBuilder } from "./schemaBuilder.ts";
 import municipality from "./schemas/municipality.ts";
 import prefecture from "./schemas/prefecture.ts";
 import { registerSchema, toPothos } from "./utils/schemaUtils.ts";
 
 const builder = getSchemaBuilder();
 
-// graphql に登録するオブジェクトの一覧を定義
-const objectMap = ((sb: SchemaBuilderType) => {
+const objectMap = ((sb) => {
   const { entities } = buildSchema(dbClient);
   const t = entities.types;
   return {
+    // graphql に登録するオブジェクトの一覧を定義
     prefecture: toPothos(sb, dbSchema.prefecture, t.PrefectureItem),
     municipality: toPothos(sb, dbSchema.municipality, t.MunicipalityItem),
   };
 })(builder);
 export type ObjectMap = typeof objectMap;
 
-// graphql に登録するスキーマの一覧を定義
-((sb: SchemaBuilderType) => {
-  registerSchema(sb, objectMap, [prefecture, municipality]);
-})(builder);
+((sb, objects) => {
+  registerSchema(sb, objects, [
+    // graphql に登録するスキーマの一覧を定義
+    prefecture,
+    municipality,
+  ]);
+})(builder, objectMap);
 
 const graphqlSchema = builder.toSchema();
 export default graphqlSchema;
