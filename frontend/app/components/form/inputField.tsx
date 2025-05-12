996a21127ffab9bc1e006bf1fa037bf4fb4a13de
naoki.okamoto <ygkiyo@okana.jp>
2025-05-12 23:31:19 +0900

    フォーム系コンポーネントを div ベースに変更

--- frontend/app/components/form/inputField.tsx
+++ frontend/app/components/form/inputField.tsx
@@ -1,4 +1,5 @@
 import { FormControl, FormField, FormItem, FormLabel, FormMessage, Input } from "@my-app/shadcn";
+import type { ComponentProps } from "react";
 import type { FieldValues, Path, UseFormReturn } from "react-hook-form";
 
 type Props<F extends FieldValues> = {
@@ -7,24 +8,26 @@ type Props<F extends FieldValues> = {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   form: UseFormReturn<F, any, F>;
   name: Path<F>;
-};
+} & ComponentProps<"div">;
 
-export function InputField<F extends FieldValues>({ label, form, name, type }: Props<F>) {
+export const InputField = <F extends FieldValues>({ label, form, name, type, ...props }: Props<F>) => {
   return (
-    <FormField
-      control={form.control}
-      name={name}
-      render={({ field }) => {
-        return (
-          <FormItem>
-            {label && <FormLabel>{label}</FormLabel>}
-            <FormControl>
-              <Input type={type} {...field} />
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        );
-      }}
-    />
+    <div {...props}>
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => {
+          return (
+            <FormItem>
+              {label && <FormLabel>{label}</FormLabel>}
+              <FormControl>
+                <Input type={type} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          );
+        }}
+      />
+    </div>
   );
-}
+};
