bb21015d3452a7a94216301fb63eb92d45e9b464
naoki.okamoto <ygkiyo@okana.jp>
2025-05-18 23:15:28 +0900

    ラベル表示変更

--- frontend/app/components/form/viewonlyField.tsx
+++ frontend/app/components/form/viewonlyField.tsx
@@ -1,15 +1,16 @@
-import { Label } from "@my-app/shadcn";
+import { Label, cn } from "@my-app/shadcn";
 import type { ComponentProps } from "react";
 
 export const ViewonlyField = ({
   label,
   value,
+  className,
   ...props
 }: { label: string; value?: string | number } & ComponentProps<"div">) => {
   return (
-    <div {...props}>
-      <Label className="text-sm">{label}</Label>
-      <span className="text-xl">{value ?? ""}</span>
+    <div className={cn("pr-2 pb-2", className)} {...props}>
+      <Label className="text-sm text-(--muted-foreground)">{label}</Label>
+      <span className="text-xl pl-1">{value ?? ""}</span>
     </div>
   );
 };
