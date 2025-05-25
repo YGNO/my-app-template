ac491742c2f8ad583e327375e8b6b7116f157163
naoki.okamoto <ygkiyo@okana.jp>
2025-05-25 20:24:47 +0900

    ツリービュー：サンプル

--- frontend/app/components/grid/gridColumn.tsx
+++ frontend/app/components/grid/gridColumn.tsx
@@ -1,4 +1,4 @@
-import type { Column, IFormatters, OnEventArgs } from "slickgrid-react";
+import type { Column, Formatter, IFormatters, OnEventArgs } from "slickgrid-react";
 
 export const editActionColumn = (
   formatters: IFormatters,
@@ -24,35 +24,56 @@ export const editActionColumn = (
   };
 };
 
+const treeButton: Formatter = (_row, _cell, value, _columnDef) => {
+  if (value === "chaild") {
+    return "";
+  }
+  const icon = document.createElement("i");
+  icon.classList.add(
+    value === "open" ? "i-mdi-menu-down" : "i-mdi-menu-right",
+    "text-[28px]",
+    "text-primary",
+    "hover:text-primary/50",
+    "relative",
+    "bottom-[4px]",
+  );
+  icon.setAttribute("role", "treeRoot");
+  return icon;
+};
+
 export const treeActionColumn = (
-  formatters: IFormatters,
+  _formatters: IFormatters,
   option?: { name?: string; onClick?: (e: Event, args: OnEventArgs) => void },
 ): Column => {
   return {
-    id: option?.name ?? "edit",
-    field: option?.name ?? "edit",
+    id: option?.name ?? "treeNode",
+    field: option?.name ?? "treeNode",
     excludeFromQuery: true,
     excludeFromColumnPicker: true,
     excludeFromGridMenu: true,
     excludeFromHeaderMenu: true,
-    formatter() {
-      return "<div>何か？</div>";
-    },
-    params: {
-      iconCssClass:
-        "i-mdi-note-edit-outline hover:i-mdi-note-edit pointer text-xl" +
-        // grid.css と色合いを合わせている
-        " text-primary/60 hover:text-primary",
-    },
+    formatter: treeButton,
     minWidth: 45,
     maxWidth: 45,
-    onCellClick(e: Event, args: OnEventArgs) {
+    onCellClick(e, args) {
+      const { row, dataContext, dataView } = args;
+      if (dataContext.treeNode === "chaild") {
+        return;
+      }
+      e.preventDefault();
+      if (dataContext.treeNode === "open") {
+        dataContext.treeNode = "close";
+        const item = dataView.getItems()[row + 1];
+        dataView.deleteItem(item.code);
+        return;
+      }
+      dataContext.treeNode = "open";
       const newData = {
-        ...args.dataContext,
+        ...dataContext,
         code: self.crypto.randomUUID(),
+        treeNode: "chaild",
       };
-      args.dataView.insertItem(args.row + 1, newData);
-      console.log("イベント", { e, args });
+      dataView.insertItem(row + 1, newData);
     },
   };
 };
